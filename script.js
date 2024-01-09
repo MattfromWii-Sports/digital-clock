fa7c71f07ec5726130ac9c6c6d860cf6ad897977
Matt <matthewjosefcruz@gmail.com>
2024-01-09 22:18:50 +0800

    Create Background Image Control Module, And Shuffling And Time Interval Function Inside

--- script.js
+++ script.js
@@ -32,4 +32,43 @@ const updateDateTime = () => {
     }, 1000);
 }
 
+const backgroundControl = (() => {
+    let interval = null;
+    let timeInterval = 0;
+    const morningRef = ['morning1.jpg', 'morning2.jpg'];
+    const afternoonRef = ['afternoon1.jpg', 'afternoon2.jpg'];
+    const eveningRef = ['evening1.jpg', 'evening2.jpg'];
+    const nightRef = ['night1.jpg', 'night2.jpg', 'night3.webp'];
+
+    //Functions:
+    const shuffle = ((array) => {
+        for(let i = array.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i+1));
+            [array[i], array[j]] = [array[j], array[i]];
+        }
+        return array;
+    });
+    //Morning = 0, Afternoon = 1, Evening = 2, Night = 3
+    const determineTimeInterval = ((x) => {
+        if(x >= 5 && x < 12) return 0;
+        else if (x >= 12 && x < 5) return 1;
+        else if (x >= 5 && x <= 7) return 2;
+        else return 3;
+    });
+    const changeBackground = () => {
+
+    }
+    const startInterval = () => {
+
+    };
+    const endInterval = () => {
+
+    }
+
+    //Code Run:
+    timeInterval = determineTimeInterval(new Date().getHours());
+    console.log(timeInterval);
+
+})();
+
 updateDateTime();
